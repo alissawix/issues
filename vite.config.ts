33147a4becc67d05973e197d8914f52bfde850f2
alissawix <alissav@wix.com>
2023-12-18 10:23:26 +0200

    use codux config alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [tsconfigPaths(), react(), svgr()],
+    plugins: [react(), svgr()],
+    resolve: {
+        alias: [
+            {
+                find: '@comps',
+                replacement: path.resolve(__dirname, 'src/components'),
+            },
+        ],
+    },
 });
